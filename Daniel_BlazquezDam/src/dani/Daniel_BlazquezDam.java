14a675f934fbdf9604109ee6a13803c534e94060
dani <dblazquezg09@iesalbarregas.es>
2021-03-16 18:20:59 +0100

    Nuevas Lineas en el main

--- Daniel_BlazquezDam/src/dani/Daniel_BlazquezDam.java
+++ Daniel_BlazquezDam/src/dani/Daniel_BlazquezDam.java
@@ -4,5 +4,7 @@ public class Daniel_BlazquezDam {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo java-git");
+		System.out.println("Nuva Linea1 ");
+		System.out.println("Nueva Linea2");
 	}
 }
